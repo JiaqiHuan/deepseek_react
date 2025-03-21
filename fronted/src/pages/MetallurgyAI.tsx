b636bd5352b8e8a147a3b8a734b0ff1d2739a32e
JiaqiHuan <78956321@qq.com>
2025-03-21 18:08:07 +0800

    change4

--- fronted/src/pages/MetallurgyAI.tsx
+++ fronted/src/pages/MetallurgyAI.tsx
@@ -68,7 +68,7 @@ export default function MetallurgyAI() {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/chat", {
+      const response = await fetch("http://47.122.119.68/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
